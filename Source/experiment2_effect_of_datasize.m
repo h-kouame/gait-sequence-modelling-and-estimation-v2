b7c4a82df8459d18b24679b64dd9cb1ab9a3e482
Kouame Kouassi <mail.kouame@gmail.com>
2017-10-31 21:02:25 +0200

    Implemented experiment for accuracy vs featsize

--- Source/experiment2_effect_of_datasize.m
+++ Source/experiment2_effect_of_datasize.m
@@ -1,6 +1,6 @@
 % Effect of training data size on performance
 
-rng(8000,'twister');
+rng('shuffle','twister');
 
 % Get dataset
 proportion = 0.2;
@@ -9,13 +9,13 @@ best_feat_num = 4; %experiment_optimal_number_of_features;
 [observ_seq, state_seq, feat_names] = get_all_data(body_part);
 
 % Have multiple runs of both methods
-run_num = 9;
-accuracies = zeros(2, run_num*2);
-logliks = zeros(3, run_num*2);
+test_prop = 9;
+accuracies = zeros(3, test_prop - 1);
+logliks = zeros(3, test_prop - 1);
 % For each run, 
-for i = run_num:-1:2
+for i = test_prop:-1:1
     testdata_proportion = i/10;
-    [traindata, testdata] = splitdataset(observ_seq, state_seq, testdata_proportion);
+    [traindata, testdata] = splitdataset(observ_seq, state_seq, feat_names, testdata_proportion);
    
     %build model without dimensionality reduction
     disp('No reduction');
@@ -26,35 +26,18 @@ for i = run_num:-1:2
     [accuracies(1, i), ~, logliks(1, i)] = GmmHMMpredict(no_reduc_model, testdata);
    
     %Build model with dimensionality reduction
-%     disp('Forward Feature selection with knn');
-    data = make_data(traindata.observ, traindata.state, feat_names);
-% %     [selected_observ, feat_out] = featselect(data, best_feat_num);
-%     feat_out = {'FrontYaw'; 'BackPitch'; 'BackRoll'; 'BackYaw'};
-%     selected_observ = get_selected_features(traindata.observ, feat_names, feat_out);
-%     reduc_testdata = get_selected_features(testdata.observ, feat_names, feat_out);
-%     testdata.observ = reduc_testdata;
-%     [pi, A, phi] = BuildGmmHMM(selected_observ, traindata.state);
-%     filter_model.A = A;
-%     filter_model.phi = phi;
-%     filter_model.pi = pi;
-%     [accuracies(2, i), ~, logliks(2, i)] = GmmHMMpredict(filter_model, testdata);
+    disp('Forward Feature selection with knn');
+    filter_model = KNN_BuildGmmHMM(traindata);
+    [accuracies(2, i), ~, logliks(2, i)] = KNN_GmmHMMpredict(filter_model, testdata);
     
     %Build model with dimensionality reduction
     disp('Feature selection with SDM');
-%     [selected_observ, feat_out] = featselect(data, best_feat_num);
-    feat_out = {'magFront_cal'; 'magBack_cal'; 'magFront_cal3'; 'BackYaw'};
-    selected_observ = get_selected_features(traindata.observ, feat_names, feat_out);
-    reduc_testdata = get_selected_features(testdata.observ, feat_names, feat_out);
-    testdata.observ = reduc_testdata;
-    [pi, A, phi] = BuildGmmHMM(selected_observ, traindata.state);
-    filter_model.A = A;
-    filter_model.phi = phi;
-    filter_model.pi = pi;
-    [accuracies(3, i), ~, logliks(3, i)] = GmmHMMpredict(filter_model, testdata);
+    SDM_model = SDM_BuildGmmHMM(traindata);
+    [accuracies(3, i), ~, logliks(3, i)] = SDM_GmmHMMpredict(SDM_model, testdata);
 end
 N = size(observ_seq, 1);
 incr = 0.1*N;
-x_axis = 0.2*N:incr:0.9*N;
+x_axis = 0.1*N:incr:0.9*N;
 % Plot effect of datasize
 figure;
 plot(x_axis, fliplr(accuracies(1,:))); hold on;
@@ -62,7 +45,7 @@ plot(x_axis, fliplr(accuracies(2,:)));
 plot(x_axis, fliplr(accuracies(3,:)));
 figtitle = 'Effect on data size on accuracy';
 title(figtitle);
-xlabel('Training data proportion');
+xlabel('Training data size');
 ylabel('HMM model accuracy');
 legend({'No reduction', 'Feature selection with KNN', 'Feature selection with SDM'}, 'Location', 'NW');
 
@@ -74,7 +57,7 @@ figure;
 plot(x_axis, fliplr(logliks(1,:))); hold on;
 plot(x_axis, fliplr(logliks(2,:))); hold on;
 plot(x_axis, fliplr(logliks(3,:)));
-figtitle = 'Effect on data size on accuracy';
+figtitle = 'Effect on data size on log-likelihood';
 title(figtitle);
 xlabel('Training data proportion');
 ylabel('LogLikelihood');
--- Source/splitdataset.m
+++ Source/splitdataset.m
@@ -1,6 +1,6 @@
-function [traindata, testdata] = splitdataset(observ_seq, state_seq, proportion)  
+function [traindata, testdata] = splitdataset(observ_seq, state_seq, feat, proportion)  
     if nargin < 1
-        [observ_seq, state_seq] = get_all_data();
+        [observ_seq, state_seq, feat] = get_all_data();
         proportion = 0.1;
     end
     
@@ -13,8 +13,11 @@ function [traindata, testdata] = splitdataset(observ_seq, state_seq, proportion)
     trIdx = training(C);
     traindata.observ = observ_seq(trIdx, :);
     traindata.state = state_seq(trIdx, :);
+    traindata.feat = feat;
+    
     %test data
     teIdx = test(C); 
     testdata.observ = observ_seq(teIdx, :); 
-    testdata.state = state_seq(teIdx, :);   
+    testdata.state = state_seq(teIdx, :); 
+    testdata.feat = feat;
 end
