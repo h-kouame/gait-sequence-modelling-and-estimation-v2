9c06424e5bf84f8df8a2c550749fa4be57432cb4
Kouame Kouassi <mail.kouame@gmail.com>
2017-11-16 01:27:12 +0200

    made poster and presentation slides

---
+++ Poster/Templates/latex-beamerposter-master/bundle_for_upload.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir beamerposter
+cp README.md beamerposter.tex beamerposter.sty examples/00/example.tex themes/*.sty beamerposter
+cd beamerposter
+mv README.md README
+pdflatex beamerposter
+rm beamerposter.aux beamerposter.log 
+cd ..
+mv 
+zip -r beamerposter-$(date +%Y%m%d).zip beamerposter
